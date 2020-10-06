b3a9cab3ff3c7fade79b8a8363a9a83088ebf78e
kmix-39 <34811629+kmix-39@users.noreply.github.com>
2020-10-06 13:11:23 +0900

    Create sample_new_mark.php
    snow-monkey/template-parts/loop/entry-summary/term/term.php にこう書けば良くね？ってサンプル

---
+++ sample_new_mark.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ * @version 11.3.3
+ */
+
+use Framework\Helper;
+
+$args = wp_parse_args(
+	// phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+	$args,
+	// phpcs:enable
+	[
+		'_terms' => [],
+	]
+);
+
+if ( ! $args['_terms'] ) {
+	return;
+}
+?>
+
+<?php foreach ( $args['_terms'] as $_term ) : ?>
+	<?php
+    $_now_date = date_i18n( 'U' );
+	$_post_date = date_i18n( 'U', strtotime( get_post()->post_date ) );
+	$_range_date = date( 'U', ( $_now_date - $_post_date ) ) / 86400;
+    if ( 40 >= $_range_date ) {
+?>
+		<span class="c-entry-summary__term">新着記事（NEW）</span>
+<?php
+} else {
+?>
+<span class="c-entry-summary__term c-entry-summary__term--<?php echo esc_attr( $_term->taxonomy . '-' . $_term->term_id ); ?>">
+	<?php echo esc_html( $_term->name ); ?>
+</span>
+<?php
+}
+?>
+<?php endforeach; ?>
